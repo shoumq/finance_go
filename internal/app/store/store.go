e9800ab156c0faea1c54f06d027af011cc5f72f2
John Doe <johndoe@example.com>
2024-06-15 12:30:52 +0300

    first commit

---
+++ internal/app/store/store.go
@@ -0,0 +1,52 @@
+package store
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+// migrate -path db/migrations -database postgres://postgres:Andrew1095@localhost:5433/finbase?sslmode=disable up
+
+type Store struct {
+	config         *Config
+	db             *sql.DB
+	userRepository *UserRepository
+}
+
+func New(config *Config) *Store {
+	return &Store{
+		config: config,
+	}
+}
+
+func (s *Store) Open() error {
+	db, err := sql.Open("postgres", s.config.DatabaseURL)
+	if err != nil {
+		return err
+	}
+
+	if err := db.Ping(); err != nil {
+		return err
+	}
+
+	s.db = db
+
+	return nil
+}
+
+func (s *Store) Close() {
+	s.db.Close()
+}
+
+func (s *Store) User() *UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+
+	s.userRepository = &UserRepository{
+		store: s,
+	}
+
+	return s.userRepository
+}
